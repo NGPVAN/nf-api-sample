571dd703353f95cb3cdae78b92c1cf1cddb0ce39
Philip Sharp <psharp@nationalfield.org>
2012-02-21 12:52:37 -0500

    tabbed output

--- public/index.php
+++ public/index.php
@@ -27,6 +27,9 @@
 <head>
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
 	<title>NationalField API Sample</title>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/themes/ui-lightness/jquery-ui.css" />
 </head>
 <body>
 	<h1>NationalField API Sample</h1>
@@ -43,12 +46,73 @@
             <input type="hidden" name="action" value="logout" />
             <p><input type="submit" value="Log Out" /></p>
         <form>
-        <h2>You:</h2>
-        <pre><?php print_r($nf->api('users/me')) ?></pre>
-        <h2>Groups:</h2>
-        <pre><?php print_r($nf->api('groups')) ?></pre>
-        <h2>Roles:</h2>
-        <pre><?php print_r($nf->api('roles')) ?></pre>
+        
+        <div id="tabs">
+            <ul>
+                <li><a href="#tabs-you">You</a></li>
+                <li><a href="#tabs-roles">Roles</a></li>
+                <li><a href="#tabs-groups">Group</a></li>
+                <li><a href="#tabs-stream">Stream</a></li>
+                <li><a href="#tabs-updown">Ups & Challenges</a></li>
+            </ul>
+            <div id="tabs-you">
+                <pre><?php print_r($nf->api('users/me')) ?></pre>
+            </div>
+            <div id="tabs-stream">
+                <pre><?php print_r($nf->api('stream')) ?></pre>
+            </div>
+            <div id="tabs-updown">
+                <?php
+                    $upsandchallenges = $nf->api('upsandchallenges');
+                    $table = array();
+                    foreach ($upsandchallenges as $ud) {
+                        $userId = $ud['actor']['id'];
+                        if (!isset($table[$userId])) {
+                            $table[$userId] = array(
+                                'id' => $userId,
+                                'name' => $ud['actor']['displayName'],
+                                'counts' => array(
+                                    'ups' => 0,
+                                    'challanges' => 0,
+                                    'changes' => 0
+                                )
+                            );
+                        }
+                        if ($ud['object']['content']['up']) $table[$userId]['counts']['ups']++;
+                        if ($ud['object']['content']['down']) $table[$userId]['counts']['challanges']++;
+                        if ($ud['object']['content']['change']) $table[$userId]['counts']['changes']++;
+                    }
+                ?>
+                    <table>
+                    <tr>
+                        <th>User</th>
+                        <th>Ups</th>
+                        <th>Challenges</th>
+                        <th>Changes</th>
+                    </tr>
+                    <?php
+                        foreach($table as $row) {
+                            echo '<tr><td>' . htmlentities($row['name']) . '</td>' .
+                                 '<td>' . number_format($row['counts']['ups']) . '</td>' .
+                                 '<td>' . number_format($row['counts']['challanges']) . '</td>' .
+                                 '<td>' . number_format($row['counts']['changes']) . '</td></tr>';
+                        }
+                    ?>
+                    </table>
+            </div>
+            <div id="tabs-roles">
+                <pre><?php print_r($nf->api('roles')) ?></pre>
+            </div>
+            <div id="tabs-groups">
+                <pre><?php print_r($nf->api('groups')) ?></pre>
+            </div>
+        </div>
+        
+        <script>
+            $(function() {
+                $( "#tabs" ).tabs();
+            });
+        </script>
     <?php endif; //isAuthenticated ?>
 </body>
 </html>
